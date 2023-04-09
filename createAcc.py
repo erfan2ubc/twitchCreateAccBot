b35a6cddab7ed22141913e31e2b647c48cf04cc6
Erfan <77303475+erfan2ubc@users.noreply.github.com>
2023-04-09 03:29:53 -0700

    notSupport

--- createAcc.py
+++ createAcc.py
@@ -2,6 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import pyautogui
+import pyperclip
 import secret
 
 class Browser:
@@ -32,19 +34,53 @@ class Browser:
         button.click()
         time.sleep(1)
 
-    def create_twitch_acc(self, username: str, password: str, email:str):
+    def create_twitch_acc(self, username: str, password: str, email: str):
         self.click_button_by_text('Sign Up')
-        time.sleep(5)
+        time.sleep(1)
         self.add_input(by=By.ID, value='signup-username', text = username)
         self.add_input(by=By.ID, value='password-input', text = password)
+        time.sleep(1)
+        self.click_button_by_text('Next Step')
+        time.sleep(1)
+        self.click_button_by_text('Use email instead')
+        self.add_input(by=By.ID, value='email-input', text=email)
+        self.click_button_by_text('Next Step')
+        pyautogui.press('down')
+        pyautogui.press('tab')
+        pyautogui.press('1')
+        pyautogui.press('tab')
+        pyautogui.press('2')
+        pyautogui.press('0')
+        pyautogui.press('0')
+        pyautogui.press('0')
+        time.sleep(1)
+        # self.click_button(by=By.CLASS_NAME, value='Layout-sc-1xcs6mc-0 ktFzpA')
 
+
+    def find_email(self):
+        print('I am here')
+        time.sleep(3)
+        email = self.browser.find_element(by=By.ID, value='mail_address').get_attribute('value')
+
+        return email
+
+    def find_verification_code(self):
+        time.sleep(3)
+        verification_code_element = self.browser.find_element(by=By.XPATH, value="//span[contains(text(),'Verification Code')]/following-sibling::p")
+        print(verification_code_element.text)
+        verification_code = verification_code_element.text.strip()
+        return verification_code
 
 if __name__ == '__main__':
-    browser = Browser('/Users/erfan/twitchAccCreator/twitchCreateAccBot/chromedriver')
-    browser.open_page('https://twitch.tv/')
-    time.sleep(10)
+    browser = Browser('chromedriver.exe')
+    browser2 = Browser('chromedriver.exe')
+    browser2.open_page('https://twitch.tv/')
+    time.sleep(3)
 
-    browser.create_twitch_acc('erfan', 'erfan', 'erfan')
+    email = browser.open_page('https://10minutemail.com/')
+    email = browser.find_email()
+    time.sleep(3)
+    browser2.create_twitch_acc('goodarz12', 'goodarz@22', email)
     time.sleep(10)
 
 
